5363e0eb953a3271cc380a91c1adfe822206d97b
Rafia Raisa Prova <rafiaraisa1152@gmail.com>
2022-06-27 11:51:50 +0600

    initial commit

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div id='home'>
+        <div className='banner'>
+            <div className='banner1'>
+                <img src="https://img.freepik.com/free-psd/minimal-wedding-social-media-post-template_23-2148382904.jpg" alt="" />
+            </div>
+            <div className='banner2'>
+                <img src="https://img.freepik.com/free-vector/watercolor-boho-wedding-instagram-stories_23-2149213906.jpg" alt="" />
+            </div>
+
+
+        </div>
+        {/* <Services></Services>
+        <div>
+
+            <Photographer></Photographer> </div> */}
+    </div>
+    );
+};
+
+export default Home;
---
+++ src/Components/SignUp/SignUp.js
@@ -0,0 +1,31 @@
+import React from 'react';
+// import { Button, Form } from 'react-bootstrap';
+import './SignUp.css';
+
+const SignUp = () => {
+    return (
+        <div className='p-4 box'>
+            {/* <h2 className='mb-3'>Sign Up</h2>
+         <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    
+    <Form.Control type="email" placeholder="Enter email" />
+   
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+  
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+ 
+<div>
+<Button variant="primary" type="submit">
+    Sign up
+  </Button>
+</div>
+</Form>    */}
+        </div>
+    );
+};
+
+export default SignUp;
